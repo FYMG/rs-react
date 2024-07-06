ffc627b873d66be4ef563b87ce1fe2f25974af7f
FYMG <svinpetr18@gmail.com>
2024-07-06 17:56:40 +0300

    chore: setup vite and tests

---
+++ src/tests/setup.ts
@@ -0,0 +1,9 @@
+import { afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
+
+afterEach(() => {
+    cleanup();
+});
+
+Object.defineProperty(window, 'scrollTo', { value: () => {}, writable: true });
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { configDefaults, defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  build: {
+    emptyOutDir: true,
+    sourcemap: true,
+    target: 'es2017',
+    cssCodeSplit: true,
+  },
+  css: {
+    devSourcemap: true,
+    modules: {
+      localsConvention: 'camelCaseOnly',
+    },
+    preprocessorOptions: {
+      minimize: true,
+      sourceMap: true,
+    },
+  },
+  test: {
+    setupFiles: 'src/tests/setup.ts',
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    include: [...configDefaults.include],
+    exclude: [...configDefaults.exclude],
+    coverage: {
+      include: ['src/**/*.{js,ts,jsx,tsx}'],
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+  plugins: [react()],
+})
